d90f2ebf5faf2e045256be33c25ce6ed299e7d28
vovka <sch.icq@gmail.com>
2012-09-19 19:27:00 +0300

    Another one type fixed

--- assets/www/js/mobilecordo/config/environment.js
+++ assets/www/js/mobilecordo/config/environment.js
@@ -1,4 +1,4 @@
-g// Loading the application file (the main file) and pass to initializer the
+// Loading the application file (the main file) and pass to initializer the
 // name of the application main namespace. It should be the same as base path
 // of the application.
 define([
